fedaa64dad15c2cf5a2cc1cc6485db823a52269d
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-13 19:25:55 +0200

    виклик конструктора батьківського класу

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-#успадкування
-class Animal:
-    def sound(self):
-        print("Гучно оре")
-class Dog(Animal):
-    def sound(self):
-        super().sound()
-        print("Гавкати")
-dog = Dog()
-dog.sound()
+class Parent:
+    def __init__(self, name):
+        self.name = name
+        print("Корисна річ")
+class Child(Parent):
+    def __init__(self, name, additional_info):
+        #self.name = name
+        super().__init__(name)#виклик конструктора батьківського класу
+        self.additional_info = additional_info
+
+child = Child("Oleg", "informaion")
+print(child.name, child.additional_info)
