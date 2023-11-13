141aba622c928e74599e071b2cbd64a3a7568c8d
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-13 20:48:23 +0200

    працює, якщо треба доповніть

--- main.py
+++ main.py
@@ -1,41 +1,33 @@
-#практика
-class Person:
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
+"""
+Завдання 4
+Створіть базовий клас Clock, який містить атрибути
+години та хвилини. Від цього базового класу
+успадковуйте два класи: AnalogClock та DigitalClock.
+Клас AnalogClock повинен мати метод display_time,
+який виводить поточний час у форматі
+"години:хвилини". Клас DigitalClock повинен мати
+метод display_time, який виводить поточний час у
+цифровому форматі "гг:хх".
+Створіть об'єкти кожного класу та виведіть
+поточний час за допомогою методу display_time.
+"""
+class Clock:
+    def __init__(self, hours, minutes):
+        self.hours = hours
+        self.minutes = minutes
 
-class Student(Person):
-    def __init__(self, name, age, gender, __student_id):
-        super().__init__(name, age, gender)
-        self.__student_id = __student_id
+    def display_times(self):
+        print(f'{self.hours:02d}:{self.minutes:02d}')
 
-    def get_student_id(self):
-        return self.__student_id
+class AnalogClock(Clock):
+    def display_times(self):
+        super().display_times()
 
-    def display_subject(self, subjects):
-        print(f"Студент {self.name} вивчив такі предмети: ")
-        for subject in subjects:
-            print(subject)
+class DigitalClock(Clock):
+    def display_times(self):
+        super().display_times()
 
-class Teacher(Person):
-    def __init__(self, name, age, gender, __employee_id):
-        super().__init__(name, age, gender)
-        self.__employee_id = __employee_id
-
-    def get_employee_id(self):
-        return self.__employee_id
-
-    def add_grades(self, student, grades):
-        print(f'Вчитель {self.name} виставив оцінку для {student.name}')
-        for subject, grade in grades.items():
-            print(f"по предмету {subject} оцінка {grade}")
-
-student1 = Student("Oleg", 15, "male", 34344)
-student2 = Student("Olga", 16, "female", 342435)
-print(f"Студент на ім'я {student1.name} з id:{student1.get_student_id()}")
-student1.display_subject(['Math', 'Science', "History"])
-
-teacher1 = Teacher('пан Михайло', 35, "male", 3456789)
-teacher1.add_grades(student1, {"Math": "A", "Science": "B", "History": "E"})
-
+analog_clock = AnalogClock(1, 34)
+digital_clock = DigitalClock(15, 43)
+analog_clock.display_times()
+digital_clock.display_times()
