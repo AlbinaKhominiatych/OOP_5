75192c1a899bcd1e513767d59faf90197318c259
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-13 19:49:40 +0200

    Методи в цьому класі повинні дозволяти встановлювати та отримувати ідентифікаційний номер студента, а також виводити повідомлення про вивчені предмети.

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ class Person:
         self.gender = gender
 
 class Student(Person):
-    def __init__(self, name, age, gender, student_id):
+    def __init__(self, name, age, gender, __student_id):
         super().__init__(self, name, age, gender)
-        self.student_id = student_id
+        self.student_id = __student_id
+
+    def get_student_id(self):
+        return self.__student_id
+
+    def display_subject(self, subjects):
+        print(f"Студент {self.name} вивчив такі предмети: ")
+        for subject in subjects:
+            print(subject)
 
 class Teacher(Person):
     def __init__(self, name, age, gender, employee_id):
