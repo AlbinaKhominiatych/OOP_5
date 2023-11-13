020a7a82c87e9298c8ebf9e2520880d9f820ea75
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-13 19:44:55 +0200

    Клас Teacher повинен також успадковувати властивості Person і мати додатковий атрибут employee_id

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ class Person:
         self.name = name
         self.age = age
         self.gender = gender
+
 class Student(Person):
     def __init__(self, name, age, gender, student_id):
         super().__init__(self, name, age, gender)
         self.student_id = student_id
+
+class Teacher(Person):
+    def __init__(self, name, age, gender, employee_id):
+        super().__init__(self, name, age, gender)
+        self.employee_id = employee_id
